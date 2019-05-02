feee7e4cab4ec91c70514b9ceb2d484d813b9150
Rachele Suy <rachelesuy@gmail.com>
2019-05-02 15:57:32 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,4 @@
+
 #numbers=[1,2,3,5]
 def my_each(array) # put argument(s) here
   # code here
@@ -10,3 +11,22 @@ def my_each(array) # put argument(s) here
   end
   return array
 end
+=======
+numbers=[1,2,3,5]
+def my_each(array) # put argument(s) here
+  # code here
+  i=0
+  while i < array.length
+ 
+   yield array[i]
+  
+  i = i + 1
+  end
+  return array
+
+end
+ my_each(numbers) do |i|
+        i
+ end
+ my_each("puts")
+
